f9e9fabfcc70da033e351e7f79d92449d44bd5c9
gerberdings <sandra.gerberding@haufe-lexware.com>
2021-09-13 21:51:10 +0200

    Cherry picked commit "talk order by id"

--- application/rating-herbstcampus/src/main/java/blog/gerberding/sandra/rating/talk/TalkRepository.java
+++ application/rating-herbstcampus/src/main/java/blog/gerberding/sandra/rating/talk/TalkRepository.java
@@ -3,6 +3,9 @@ package blog.gerberding.sandra.rating.talk;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TalkRepository extends CrudRepository<Talk, Long> {
+  List<Talk> findAllByOrderByIdAsc ();
 }
