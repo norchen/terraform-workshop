7f1ea2124eb67f14dbdc4fcfbfab2aadb2d6c1a0
Nora Schöner <nora.schoener@codecamp-n.com>
2021-09-14 21:36:24 +0200

    add healthcheck for loadbalancer

--- infrastructure/_final/main.tf
+++ infrastructure/_final/main.tf
@@ -119,6 +119,7 @@ resource "aws_security_group" "server" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.loadbalancer.id]
   }
 }
 
@@ -242,6 +243,12 @@ resource "aws_lb_target_group" "server" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_default_vpc.default.id
+
+  health_check {
+    enabled = true
+    path = "/actuator/health"
+    port = 80
+  }
 }
 
 resource "aws_lb_target_group_attachment" "server" {
